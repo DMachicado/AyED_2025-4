68974ed3b4eb2a8265bd72d4f86e151f68ad3385
DMachicado <brianmachicado408@gmail.com>
2025-04-29 10:52:23 -0300

    Add files via upload

---
+++ 4_Machicado/1_PC4_1/ConsoleApplication1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal c, f;
+            Console.WriteLine("ingrese la cantidad de celsius");
+            c = Convert.ToDecimal(Console.ReadLine());
+            f = (c * 9 / 5) + 32;
+            Console.WriteLine("el cambio a farenheit es: " + f + "°");
+            Console.ReadKey();
+        }
+    }
+}
