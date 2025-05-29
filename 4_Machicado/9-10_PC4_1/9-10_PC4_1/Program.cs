ea74b0b2864184e511d6fa5b37f64501ca7ad275
DMachicado <brianmachicado408@gmail.com>
2025-05-29 10:38:30 -0300

    Add files via upload

---
+++ 4_Machicado/9-10_PC4_1/9-10_PC4_1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _9_10_PC4_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int altura, j;
+            Console.WriteLine("ingrese la altura");
+            altura = Convert.ToInt32(Console.ReadLine());
+            j = 0;
+            for (int i = altura; i > j;)
+            {
+                j++;
+                for (int t = 0; t <= j; t++)
+                {
+                    if (t != j)
+                    {
+                        Console.Write("*");
+                    }
+                    if (t == j)
+                    {
+                        if (i != j)
+                        {
+                            Console.WriteLine();
+                        }
+                    }
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
