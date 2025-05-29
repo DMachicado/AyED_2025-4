2a093e5a5e06839615821359dff95ce0499d84e0
DMachicado <brianmachicado408@gmail.com>
2025-05-29 10:40:32 -0300

    Add files via upload

---
+++ 4_Machicado/11_PC4_1/11_PC4_1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _11_PC4_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double num1, num2;
+            //Pies
+            Console.WriteLine("Ingresa la longitud");
+            num1 = Convert.ToInt32(Console.ReadLine());
+            num2 = num1;
+            num1 = pies(num1);
+            Console.WriteLine("Su medida en pies es: " + num1);
+            //Pulgadas
+            num2 = pulgadas(num2);
+            Console.WriteLine("Su medida en pulgadas es: " + num2);
+            Console.ReadKey();
+        }
+        public static double pies(double n)
+        {
+
+            n = n / 30.48;
+            return n;
+        }
+        public static double pulgadas(double n)
+        {
+            n = n / 2.54;
+            return n;
+        }
+
+    }
+}
