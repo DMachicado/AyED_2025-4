130e259fc7f1393f0373bc671e4706bc23e2e7c9
DMachicado <brianmachicado408@gmail.com>
2025-05-29 10:41:57 -0300

    Add files via upload

---
+++ 4_Machicado/12_PC4_1/12_PC4_1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _12_PC4_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string plb;
+            Console.WriteLine("ingrese una palabra/frase");
+            plb = Console.ReadLine();
+            int i = 0;
+            foreach (char letra in plb)
+            {
+                if (letra == 'a' || letra == 'e' || letra == 'i' || letra == 'o' || letra == 'u')
+                {
+                    i++;
+                }
+                if (letra == 'A' || letra == 'E' || letra == 'I' || letra == 'O' || letra == 'U')
+                {
+                    i++;
+                }
+            }
+            Console.WriteLine("La palabra/frase contiene " + i + " vocales");
+            Console.ReadKey();
+        }
+    }
+}
